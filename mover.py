0578ab529fe7bbe5cec930aa8661c00285bf6a8f
bacels <53403085+bacels@users.noreply.github.com>
2022-06-19 11:53:50 -0300

    Add files via upload

--- mover.py
+++ mover.py
@@ -1,11 +1,12 @@
 import os
 
-caminho = r"C:\Users\Vitor Barcelos\Desktop"
+caminho = r"G:/Trabalhos/Projetos"
+destino = r"C:/Users/Vitor/Desktop"
 lista_arquivos = os.listdir(caminho + '\Save_Videos')
 
 for arquivo in lista_arquivos:
     if '.avi' in arquivo:
-        os.rename(caminho + f"/Save_Videos/{arquivo}", caminho + f"/Save_Videos/Jan/{arquivo}")
+        os.rename(caminho + f"/Save_Videos/{arquivo}", destino + f"/Save_Videos2/{arquivo}")
                 # caminho da pasta + f (para dizer que tem uma variavel dentro de aspas do texto) + Nome da pasta onde se encontram os arquivos + nome do arquivo
                 # , a mesma coisa porem com o Novo caminho da onde o arquivo tem que ir (no caso aqui a pasta Jan)
-        print(cam_comp +  arquivo)
+        print(destino +  arquivo)
